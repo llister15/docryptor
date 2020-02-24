f9340a4a2024ef5213c32277e00a7dd287026e3b
llister15 <llister@wonkasoft.com>
2020-02-24 03:31:34 -0800

    updated users

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,49 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class PagesController extends Controller {
+class PagesController extends Controller
+{
 
-	public function index() {
-		$title = 'Home';
-		return view( 'pages.index' )->with( 'title', $title );
-	}
+    public function index()
+    {
+        $title = 'Home';
+        return view('pages.index')->with('title', $title);
+    }
 
-	public function dashboard() {
-		$title = 'Dashboard';
-		return view( 'pages.dashboard' )->with( 'title', $title );
-	}
+    public function dashboard()
+    {
+        $title = 'Dashboard';
+        return view('pages.dashboard')->with('title', $title);
+    }
 
-	public function upload() {
-		$title = 'Upload';
-		return view( 'pages.upload' )->with( 'title', $title );
-	}
+    public function upload()
+    {
+        $title = 'Upload';
+        return view('pages.upload')->with('title', $title);
+    }
 
-	public function about() {
-		$title = 'About';
-		return view( 'pages.about' )->with( 'title', $title );
-	}
+    public function about()
+    {
+        $title = 'About';
+        return view('pages.about')->with('title', $title);
+    }
 
-	public function review() {
-		$title = 'Review';
-		return view( 'pages.review' )->with( 'title', $title );
-	}
+    public function review()
+    {
+        $title = 'Review';
+        return view('pages.review')->with('title', $title);
+    }
 
-	public function users() {
-		$title = 'Users';
-		return view( 'pages.users' )->with( 'title', $title );
-	}
-
-
-	public function reports() {
-			$title = 'Reports';
-			return view( 'pages.reports' )->with( 'title', $title );
-	}
+    public function users()
+    {
+        $users = DB::table('users')->get();
+        return view('pages.users')->with('users', $users);
+    }
 
 
-	public function integrations() {
-			$title = 'Integrations';
-			return view( 'pages.integrations' )->with( 'title', $title );
-	}
+    public function reports()
+    {
+            $title = 'Reports';
+            return view('pages.reports')->with('title', $title);
+    }
 
+
+    public function integrations()
+    {
+            $title = 'Integrations';
+            return view('pages.integrations')->with('title', $title);
+    }
 }
--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -11,7 +11,18 @@ active
 @section('content')
 
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-    <h1 class="h1">{{$title}}</h1>
+    <h1 class="h1">Users</h1>
   </div>
+  @if(count($users) > 0 )
+  	@foreach($users as $user)
+  		<div class="well">
+  			<h3>{{ $user->name }}</h3>
+  			<h4>{{ $user->email }}</h4>
+  			<small>Posted on {{ $user->created_at }}</small>
+  		</div>
+  	@endforeach
+  @else
+  	<p>No users found</p>
+  @endif
      
 @endsection
